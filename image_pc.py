692b5138fbb7692c07541f7bb92f7648ae7f2c72
AmelyneFernandes <amelyne.fernandes@gmail.com>
2024-05-07 17:36:30 -0300

    sexto commit

--- image_pc.py
+++ image_pc.py
@@ -3,7 +3,7 @@ from kivy.uix.image import Image
 
 class MinhaApp(App):
     def build(self):
-        return Image(source= "")
+        return Image(source= r'C:/Users/aluno.sesipaulista/Desktop/aplicativo/fotopc/Merida01.webp')
     
 if __name__ == "__main__":
     MinhaApp().run()
--- image_web.py
+++ image_web.py
@@ -1,10 +1,15 @@
 from kivy.app import App
-from kivy.uix.image import Image, AsyncImage
+from kivy.network.urlrequest import UrlRequest
+from kivy.uix.image import AsyncImage
+from kivy.uix.boxlayout import BoxLayout
 
 class MinhaApp(App):
     def build(self):
-
-        return AsyncImage(source="")
-    
-if __name__ == " __main__":
+        layout= BoxLayout()
+        url = 'https://recreio.uol.com.br/media/_versions/disney/merida_valente_capa_widelg.jpg'
+        image= AsyncImage(source=url)
+        layout.add_widget(image)
+        return layout 
+        
+if __name__ == "__main__":
     MinhaApp().run()
